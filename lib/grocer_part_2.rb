116c4599510c1830889df032fbfc1950e419b6d0
Yunhao Gao <as510891328@gmail.com>
2020-08-21 03:24:48 +0000

    Automatically backed up by Learn

--- lib/grocer_part_2.rb
+++ lib/grocer_part_2.rb
@@ -40,5 +40,5 @@ def checkout(cart, coupons)
   # BEFORE it begins the work of calculating the total (or else you might have
   # some irritated customers
   
-  con_cart = consolidate_cart(cart)
+  binding.pry
 end
